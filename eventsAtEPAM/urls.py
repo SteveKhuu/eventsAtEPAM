143a1111fccb122776ad787135bad3c00f7a2029
SteveKhuu <Stephen_Khuu@epam.com>
2012-12-10 17:34:47 -0500

    added subscriber preferences model

--- eventsAtEPAM/urls.py
+++ eventsAtEPAM/urls.py
@@ -17,7 +17,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^$', views.index, name='index'),
+    
     url(r'^my_events/$', views.my_events, name='my_events'),
+    url(r'^my_preferences/$', views.my_preferences, name='my_preferences'),
     
     url(r'^(?P<event_id>\d+)/$', views.detail, name='detail'),
     url(r'^attend/(?P<event_id>\d+)/$', views.attend_event, name='attend'),
@@ -26,8 +28,10 @@ urlpatterns = patterns('',
     url(r'^edit/(?P<event_id>\d+)/$', views.edit_event, name='edit'),
     url(r'^export/(?P<event_id>\d+)/$', views.export_event, name='export'),
     url(r'^comment/(?P<event_id>\d+)/$', views.comment, name='comment'),
+    
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/profile/', redirect_to, {'url': '/'}),
+
     url(r'^admin/', include(admin.site.urls)),
     
 )
